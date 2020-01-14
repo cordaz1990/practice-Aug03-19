2d0ea4e98b76a4058e87a53c8d5c2d5f0660b503
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:09:23 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
-print('hello')
-print('world')
-print('world')
+#deleting elements
+
+>>> t = ['a','b', 'c']
+x = t.pop(1)
+>>> t
+['a', 'c']
+
+>>> t = ['a','b','c']
+>>> t.remove('b')
+>>> t
+['a','c']
+
+>>> t = ['a','b','c','d','e','f']
+del t[1;5]
+>>> t
+['a', 'f']
